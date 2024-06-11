620af5b6f6ac12aa602bc3ec48418250575e16ef
Sofyan Mahmoud <sofyan1020@gmail.com>
2024-06-12 02:40:47 +0300

    Fix: Remove the unused variables

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,8 +1,5 @@
 import React from 'react'
 
-import { experimentalStyled as styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import HomeIcon from '@mui/icons-material/Home';
